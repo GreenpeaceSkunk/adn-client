b9bc85654e8cd238e9311a0e7cf1409b58f14012
Dan Tovbein <dtovbein@gmail.com>
2021-03-17 19:25:17 -0300

    Fix click handler on overlay element

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -77,9 +77,7 @@ const Component: React.FunctionComponent<IProps> = ({
           </Wrapper>
         </Wrapper>
       </Wrapper>
-      <Overlay
-        onClick={goBack}
-      />
+      <Overlay/>
     </>
   ), [
     children,
