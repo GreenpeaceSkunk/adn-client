69729aaa02cdd8e52c035ef9d7a16ddf82e44c5c
Dan Tovbein <dtovbein@gmail.com>
2021-05-05 14:36:06 -0300

    Modify icons implementation and add Arrow right icon

---
+++ src/utils/icons.ts
@@ -0,0 +1,21 @@
+import GreenpeaceLogoWhite from '../assets/icons/greenpeace-white.svg';
+import GreenpeaceLogoGreen from '../assets/icons/greenpeace-green.svg';
+import FacebookLogo from '../assets/icons/facebook.svg';
+import TwitterLogo from '../assets/icons/twitter.svg';
+import WhatsappLogo from '../assets/icons/whatsapp.svg';
+import EmailLogo from '../assets/icons/email.svg';
+import PeopleIcon from '../assets/icons/people.svg';
+import SelectArrowIcon from '../assets/icons/select-arrow.svg';
+import ArrowRightIcon from '../assets/icons/arrow-right.svg';
+
+export {
+  GreenpeaceLogoWhite,
+  GreenpeaceLogoGreen,
+  FacebookLogo,
+  TwitterLogo,
+  WhatsappLogo,
+  EmailLogo,
+  PeopleIcon,
+  SelectArrowIcon,
+  ArrowRightIcon,
+}
