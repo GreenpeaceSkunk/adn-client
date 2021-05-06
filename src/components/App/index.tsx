378d5e637043aa4d236133d72084f840a9beb740
Dan Tovbein <dtovbein@gmail.com>
2021-05-06 14:36:16 -0300

    Add env detection for third party libraries

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -6,16 +6,15 @@ import {DarkTheme as Theme} from '../../theme/Theme';
 import { Wrapper } from '@bit/meema.ui-components.elements';
 import ErrorBoundary from '../ErrorBoundary';
 import { AppContext, AppProvider } from './context';
+import { BackgroundHome } from '../../assets/images';
+import { animateScroll as scroll } from 'react-scroll';
+import AppRouter from './router';
 import { pushToDataLayer } from '../../utils/gtm';
 import { trackEvent } from '../../utils/facebookPixel';
 import { initialize as initializeTagManager } from '../../utils/gtm';
 import { initialize as initializeFacebookPixel } from '../../utils/facebookPixel';
-import { BackgroundHome } from '../../assets/images';
-import { animateScroll as scroll } from 'react-scroll';
 
-import AppRouter from './router';
-
-if(!process.env.DEBUG_MODE) {
+if(process.env.REACT_APP_DEBUG_MODE === 'false') {
   initializeTagManager();
   initializeFacebookPixel();
 }
@@ -23,14 +22,15 @@ if(!process.env.DEBUG_MODE) {
 const Component: React.FunctionComponent<{}> = () => {
   const { searchParams } = useContext(AppContext);
   const { pathname } = useLocation();
-
+  
   useEffect(() => {
     scroll.scrollToTop({
       duration: 250,
       smooth: 'easeInOutQuart',
     });
-
-    if(!process.env.DEBUG_MODE) {
+    
+    if(process.env.REACT_APP_DEBUG_MODE === 'false') {
+      console.log('Entra')
       trackEvent('PageView');
       pushToDataLayer('pageview');
     }
